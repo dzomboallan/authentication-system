59c6f45d419af3de7a0b1ffe9c0dc3145ff9069b
dzomboallan <dzomboallan1809@gmail.com>
2024-02-05 16:29:20 +0300

    Created frontend API endpoints for users

--- django_rest_auth/settings.py
+++ django_rest_auth/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt.token_blacklist",
     "accounts.apps.AccountsConfig",
     "social_accounts.apps.SocialAccountsConfig",
     "corsheaders",
